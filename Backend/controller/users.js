a87c76c5c6441c9ba7d0298e0baa19c2fbdc0184
Vivek Singh <rogerthatvivek@gmail.com>
2022-10-26 16:07:13 +0530

    Added bCrypt Password Hashing

--- Backend/controller/users.js
+++ Backend/controller/users.js
@@ -1,29 +1,35 @@
 const Users=require('../model/users')
-
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 exports.createUser=(req, res, next)=>{
-    Users.findOrCreate({
-        where: {email:req.body.email},
-        defaults: {
-            name: req.body.name,
-            password: req.body.password
-    }}).then(response=>{
-        res.status(201).send(response)
-    }).catch(err=>console.log(err))
+    console.log(req.body)
+    bcrypt.hash(req.body.password, saltRounds).then((hash)=>{
+        console.log(hash)
+        Users.findOrCreate({
+            where: {email:req.body.email},
+            defaults: {
+                name: req.body.name,
+                password: hash
+        }}).then(response=>{
+            res.status(201).send(response)
+        }).catch(err=>console.log(err))
+    });
 }
 
 exports.findUser=(req, res, next)=>{
-    console.log(req.params.creds)
     const creds=JSON.parse(req.params.creds)
-    console.log(typeof(creds))
     Users.findOne({where: {email:creds.email}})
     .then(response=>{
         if (response==null || response==''){
             res.status(200).send({code:0})
-        }else if(response.password==creds.password){
-            res.status(200).send({code:1})
         }else{
-            res.status(200).send({code:2})
+            bcrypt.compare(creds.password, response.password).then((result)=>{
+                if(result){
+                    res.status(200).send({code:1})
+                }else{
+                    res.status(200).send({code:2})
+                }
+            });
         }
-        res.status(200).send(response)
     }).catch(err=>console.log(err))
 }
